7597b48d0594a446f4d7292f1c987e3b6629dcb6
Ryan Hellwege <rhellwege6@gmail.com>
2024-12-09 22:28:21 -0800

    start skills

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -10,24 +10,50 @@
     <div class="divider"></div>
     <section>
         <h2>Programming Languages</h2>
-        <p></p>
-        <div class="divider"></div>
+        <ul>
+            <li>C/C++</li>
+            <li>Javascript/Typescript</li>
+            <li>Go</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>Rust</li>
+        </ul>
     </section>
+    <div class="divider"></div>
     <section>
-        <h2>Frameworks</h2>
-        <p></p>
-        <div class="divider"></div>
+        <h2>Frameworks and Libraries</h2>
+        <ul>
+            <li>QT</li>
+            <li>Svelte</li>
+            <li>React</li>
+            <li>SolidJS</li>
+            <li>Flask</li>
+            <li>Pytorch</li>
+        </ul>
     </section>
+    <div class="divider"></div>
     <section>
         <h2>Other Technical Skills</h2>
-        <p></p>
-        <div class="divider"></div>
+        <ul>
+            <li>Linux / Unix Systems</li>
+            <li>Docker deployment</li>
+            <li>Git versioning</li>
+            <li>Agile Development</li>
+            <li>Debugging (source level and assembly level)</li>
+            <li>Reverse Engineering</li>
+        </ul>
     </section>
+    <div class="divider"></div>
     <section>
         <h2>Soft Skills</h2>
-        <p></p>
-        <div class="divider"></div>
+        <ul>
+            <li>Active listener</li>
+            <li>Leadership</li>
+            <li>Patience</li>
+            <li>Ethical Responsibility</li>
+        </ul>
     </section>
+    <div class="divider"></div>
 </article>
 
 <style>
