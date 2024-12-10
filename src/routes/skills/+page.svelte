b533d4d219e85e625cbc0fc1aac15786e87f9599
Ryan Hellwege <rhellwege6@gmail.com>
2024-12-09 22:19:31 -0800

    skills page skeleton

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -2,13 +2,36 @@
 </script>
 
 <svelte:head>
-    <title>Ryan Hellwege Portfolio | Home</title>
+    <title>Ryan Hellwege Portfolio | Skills</title>
 </svelte:head>
 
-<h1>Home</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<article id="main" class="prose mx-auto text-lg">
+    <h1 id="title" class="font-bold text-3xl pt-5">Skills</h1>
+    <div class="divider"></div>
+    <section>
+        <h2>Programming Languages</h2>
+        <p></p>
+        <div class="divider"></div>
+    </section>
+    <section>
+        <h2>Frameworks</h2>
+        <p></p>
+        <div class="divider"></div>
+    </section>
+    <section>
+        <h2>Other Technical Skills</h2>
+        <p></p>
+        <div class="divider"></div>
+    </section>
+    <section>
+        <h2>Soft Skills</h2>
+        <p></p>
+        <div class="divider"></div>
+    </section>
+</article>
 
 <style>
+    #main {
+        width: 70vw;
+    }
 </style>
