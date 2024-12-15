1d629512565cc27bc7840a0f19ebe40406938f7d
Ryan Hellwege <rhellwege6@gmail.com>
2024-12-15 11:43:52 -0800

    finish ai waste recog

--- src/routes/projects/aiwasterecognition/+page.svelte
+++ src/routes/projects/aiwasterecognition/+page.svelte
@@ -1,58 +1,74 @@
 <script>
+    import BrowserDemo from "$lib/components/browserdemo/BrowserDemo.svelte";
 </script>
 
 <svelte:head>
     <title>Ryan Hellwege Portfolio | AI Waste Recognition</title>
 </svelte:head>
 
-<div id="main" class="mx-auto text-lg">
+<div id="main" class="mx-auto text-lg text-center">
     <h1 id="title" class="font-bold text-5xl text-center py-5">
         AI Waste Recognition
     </h1>
+    <div class="divider"></div>
     <section class="">
-        <h2 class="text-center text-2xl font-bold mb-5">
-            Demo <a class="link" target="_blank" href="https://ai.rhellwege.xyz"
+        <BrowserDemo
+            title="AI Waste Recognition Demo "
+            link="https://ai.rhellwege.xyz"
+            width={800}
+            height={800}
+            ><a target="_blank" href="https://ai.rhellwege.xyz" class="link"
                 >https://ai.rhellwege.xyz</a
-            >
-        </h2>
-        <div class="content flex flex-col justify-center align-center">
-            <iframe
-                title="synnim-wasm"
-                src="https://ai.rhellwege.xyz"
-                frameborder="0"
-                scrolling="no"
-                class="mx-auto"
-            >
-                ></iframe
-            >
-            <div class="p-3" id="small-device">
-                <div class="mb-1 skeleton w-48 h-32"></div>
-                <h2 class="text-center text-error">
-                    Your Device is too small for the demo
-                </h2>
-            </div>
-        </div>
+            ></BrowserDemo
+        >
     </section>
     <div class="divider"></div>
-    <section class="prose">
-        <h2>Overview</h2>
+    <section class="">
+        <h2 class="font-bold text-2xl mb-2">Overview</h2>
+        <p>
+            This project was for my Software Engineering class at CSUF (CPSC
+            362). The purpose of this project was to utilize the agile framework
+            to collaborate with teammates and create a functioning project. Our
+            team decided to create an AI classifier. I decided to train my own
+            classifier from scratch and make it interactive so people can see
+            what the weights look like and also feed it their own images to
+            either classify or live train.
+        </p>
     </section>
     <div class="divider"></div>
-    <section class="prose">
-        <h2>How to Use</h2>
+    <section class="">
+        <h2 class="font-bold text-2xl mb-2">How to Use</h2>
+        <p>
+            I made sure the UI was as simple as possible. The left window shows
+            how well the model performs on training data and the right side is
+            the user controls. You start by uploading a picture by pressing the
+            choose file button. Then, the server will feed the image to the
+            model and send back the results. It will respond with a percentage
+            and what it thinks the image contains. After this, you have the
+            choice to correct the model. You can set the learning rate yourself,
+            but if you set it too high, it will make the model perform worse.
+        </p>
     </section>
     <div class="divider"></div>
-    <section class="prose">
-        <h2>Technical Details</h2>
+    <section class="">
+        <h2 class="font-bold text-2xl mb-2">Technical Details</h2>
+        <p>
+            In order to implement the model I used pytorch. Since I wanted the
+            model to be made from scratch, I had to come up with the
+            architecture myself. I used a simple CNN as a starting point. I got
+            the training data from kaggle and experimented with different
+            parameters. It took me a while to find the right parameters since I
+            was training everything from my laptop with a semi-decent graphics
+            card. Once I found the right model, I saved it then proceeded to set
+            up the server. I decided to dockerize the app because pytorch is
+            hard to set up on local machines. It is also nice to containerize
+            because now I can deploy the app from wherever I want.
+        </p>
     </section>
     <div class="divider"></div>
 </div>
 
 <style>
-    iframe {
-        width: 800px;
-        height: 650px;
-    }
     @font-face {
         font-family: "Bokor";
         src: url("/fonts/Bokor-Regular.ttf");
@@ -65,21 +81,5 @@
     }
     #main {
         width: 70vw;
-    }
-    #small-device {
-        visibility: hidden;
-        height: 0px;
-    }
-    @media (max-width: 823px) {
-        iframe {
-            visibility: hidden;
-            display: hidden;
-            height: 0px;
-        }
-        #small-device {
-            display: static;
-            visibility: visible;
-            height: auto;
-        }
     }
 </style>
